ce8e1ca50c001a6ab2e612dc29cd09a139f0e339
Anne Nabwire <nabanne96@gmail.com>
2025-02-15 20:12:07 +0300

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import { useState } from "react";
+function EyesOnMe(){
+    const [focus, setFocus]=useState(false);
+    function handleFocus(){
+        setFocus(true);
+       console.log("Good!")
+        
+}
+function handleBlur(){
+    setFocus(false);
+    console.log("Hey! Eyes on me!");
+
+}
+
+    return <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+}
+export default EyesOnMe;
